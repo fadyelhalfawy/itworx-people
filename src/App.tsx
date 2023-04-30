d2f61c290bacad9c98ab085cd28f70e7d1a3a070
Fady ElHalfawy <fadyhalfawy@gmail.com>
2023-04-30 09:56:13 +0300

    Update App file.

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,9 @@ import SinglePerson from "./components/SinglePerson";
 function App() {
   return (
    <main>
-
-     <section className={"container"}>
-       <div className={"title"}>
-         <h2>
-           Person
-         </h2>
-         <div className={"underline"}></div>
-       </div>
-
-         <SinglePerson />
-     </section>
-
+     <SinglePerson />
    </main>
+
   );
 }
 
