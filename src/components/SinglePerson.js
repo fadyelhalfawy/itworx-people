5f35f45cd17a76b83c6bc123f160e99219481b00
Fady ElHalfawy <fadyhalfawy@gmail.com>
2023-04-30 09:52:14 +0300

    Update SinglePerson file by adding it's own css file.

--- src/components/SinglePerson.js
+++ src/components/SinglePerson.js
@@ -1,24 +1,34 @@
 import {useState} from "react";
 import person from "../services/TestData";
 import { FaGithub } from "react-icons/fa";
+import "../css/SinglePerson.css";
+
 const SinglePerson = () => {
     const [index, setIndex] = useState(0);
     const {name, job, image, text} = person[index];
 
     return (
-        <article className={"choice"}>
+            <section className={"container"}>
+                <div className={"title"}>
+                    <h2>
+                        Person
+                    </h2>
+                    <div className={"underline"}></div>
+                </div>
+                <article className={"choice"}>
 
-            <div className={"img-container"}>
-                <img src={image} alt={name} className={"person-img"} />
-                <span className={"quote-icon"}>
-                    <FaGithub />
-                </span>
-            </div>
+                    <div className={"img-container"}>
+                        <img src={image} alt={name} className={"person-img"} />
+                        <span className={"quote-icon"}>
+                            <FaGithub />
+                        </span>
+                    </div>
 
-            <h2 className={"author"}>{name}</h2>
-            <h3 className={"job"}>{job}</h3>
-            <p className={"info"}>{text}</p>
-        </article>
+                    <h2 className={"author"}>{name}</h2>
+                    <h3 className={"job"}>{job}</h3>
+                    <p className={"info"}>{text}</p>
+                </article>
+            </section>
     );
 }
 export default SinglePerson;
