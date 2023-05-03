fef6a25c2f5b653f5111dc43c5cfe359ecb08c27
Fady ElHalfawy <fadyhalfawy@gmail.com>
2023-05-03 15:00:00 +0300

    Create ListingPeople file which will get our people.

---
+++ src/components/ListingPeople.js
@@ -0,0 +1,20 @@
+import {Component} from "react";
+import {getPeople} from "../services/PeopleService";
+import Person from "./Person";
+export default class ListingPeople extends Component {
+    state = {
+        people: []
+    };
+    async componentDidMount() {
+        const { data } = await getPeople();
+        const people = [...data.data];
+        this.setState({ people });
+    }
+    render() {
+        const { people } = this.state;
+        return (
+            <Person people={people} />
+        );
+    }
+
+}
