e9533812ab9d4a46f11247d174be3067ddab5962
Fady ElHalfawy <fadyhalfawy@gmail.com>
2023-05-03 14:59:36 +0300

    Adding the new listing people file.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 // @ts-ignore
 import SinglePerson from "./components/SinglePerson";
+import ListingPeople from "./components/ListingPeople";
 function App() {
   return (
    <main>
-     <SinglePerson />
+       <ListingPeople />
+     {/*<SinglePerson />*/}
    </main>
 
   );
