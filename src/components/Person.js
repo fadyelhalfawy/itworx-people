159da2971d5955efad3a5edc0f46519de379792b
Fady ElHalfawy <fadyhalfawy@gmail.com>
2023-05-03 17:19:54 +0300

    Update Person file.

--- src/components/Person.js
+++ src/components/Person.js
@@ -2,15 +2,16 @@ import {Component} from "react";
 import "../css/Person.css";
 export default class Person extends Component {
     render() {
-        const { people } = this.props;
+        const { people, submit } = this.props;
         return (
-            <div className={"container"}>
+            <div className={"people"}>
                 {people.map(person => (
                     <div className={"person"} key={person.id}>
-                        <img src={person.avatar} alt={person.avatar} />
+                        <img src={person.avatar} alt={person.avatar}/>
                         <div>
                             <h4>{person.first_name + " " + person.last_name}</h4>
                             <h4>{person.email}</h4>
+                            <button className={"btn btn-outline-info btn-space"} onClick={submit}>Read More</button>
                         </div>
                     </div>
                 ))}
