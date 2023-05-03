36e69f2411f49688bb02e113fe3dab93bbdf02e1
Fady ElHalfawy <fadyhalfawy@gmail.com>
2023-05-03 17:18:42 +0300

    Update App file by adding routing.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,27 @@
-import React from 'react';
-// @ts-ignore
+import React, {Component} from 'react';
 import SinglePerson from "./components/SinglePerson";
 import ListingPeople from "./components/ListingPeople";
-function App() {
-  return (
-   <main>
-       <ListingPeople />
-     {/*<SinglePerson />*/}
-   </main>
+import {NotFound} from "./components/NotFound";
+// @ts-ignore
+import {Redirect, Switch, Route} from "react-router-dom";
 
-  );
+export class App extends Component {
+    render() {
+        return (
+                <main>
+                    <Switch>
+                        <Route path={"/listingPeople"} component={ListingPeople}/>
+                        <Route path={"/singlePerson"} component={SinglePerson} />
+                        <Route path={"/not-found"} to={NotFound} />
+                        <Redirect from={"/"} exact to={"/listingPeople"}/>
+                        <Redirect to={"/not-found"}/>
+                    </Switch>
+
+                </main>
+
+        );
+    }
+
 }
 
 export default App;
