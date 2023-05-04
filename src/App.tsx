83b201e613c4593903afe2c6a600bcfb46dc8fb0
Fady ElHalfawy <fadyhalfawy@gmail.com>
2023-05-04 13:30:41 +0300

    Update the App file by adding Routing.

--- src/App.tsx
+++ src/App.tsx
@@ -4,25 +4,18 @@ import ListingPeople from "./components/ListingPeople";
 import {NotFound} from "./components/NotFound";
 // @ts-ignore
 import {Redirect, Switch, Route} from "react-router-dom";
-import Login from "./components/Login";
-import "react-toastify/dist/ReactToastify.css";
-import { ToastContainer } from "react-toastify";
-import ProtectedRoute from "./components/ProtectedRoute";
 
 export class App extends Component {
     render() {
         return (
                 <main>
                     <Switch>
-                        <Route path={"/login"} component={Login}/>
-                        <Route path={"/listingPeople"} component={ListingPeople} />
+                        <Route path={"/listingPeople"} component={ListingPeople}/>
                         <Route path={"/singlePerson"} component={SinglePerson} />
                         <Route path={"/not-found"} to={NotFound} />
                         <Redirect from={"/"} exact to={"/listingPeople"}/>
                         <Redirect to={"/not-found"}/>
                     </Switch>
-
-                    <ToastContainer />
 
                 </main>
 
