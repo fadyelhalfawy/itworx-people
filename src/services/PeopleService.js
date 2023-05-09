9d4740a47f2bda5164f85af27deffcd9077deb64
Fady ElHalfawy <fadyhalfawy@gmail.com>
2023-05-10 02:38:09 +0300

    Update PersonService by adding get person function and saveUpdate.

--- src/services/PeopleService.js
+++ src/services/PeopleService.js
@@ -4,3 +4,17 @@ import config from "../config/config.json";
 export function getPeople() {
     return http.get(config.peoplePath);
 }
+
+export function getPerson() {
+    return http.get(config.personPath);
+}
+export function saveUpdate(person) {
+    console.log(person);
+    if (person.id){
+        const body = { ...person };
+        delete body.id;
+        return http.put(config.personPath, body);
+    }
+
+    return http.post(config.peoplePath, person);
+}
