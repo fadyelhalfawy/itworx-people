646f9b4304e6251447f7c2f8ff723470fba8feb8
Fady ElHalfawy <fadyhalfawy@gmail.com>
2023-05-10 05:05:07 +0300

    Refract

--- src/components/Person.js
+++ src/components/Person.js
@@ -2,7 +2,7 @@ import {Component} from "react";
 import "../css/Person.css";
 export default class Person extends Component {
     render() {
-        const { people, submit, deletePerson } = this.props;
+        const { people, showPerson, deletePerson } = this.props;
         return (
             <div className={"people"}>
                 {people.map(person => (
@@ -11,8 +11,8 @@ export default class Person extends Component {
                         <div>
                             <h4>{person.first_name + " " + person.last_name}</h4>
                             <h4>{person.email}</h4>
-                            <button className={"btn btn-outline-info btn-space"} onClick={submit}>Update</button>
-                            <button className={"btn btn-outline-info btn-space m-2"} onClick={deletePerson}>Delete</button>
+                            <button className={"btn btn-outline-info btn-space"} onClick={() => showPerson(person.id)}>Update</button>
+                            <button className={"btn btn-outline-info btn-space m-2"} onClick={() => deletePerson(person.id)}>Delete</button>
                         </div>
                     </div>
                 ))}
