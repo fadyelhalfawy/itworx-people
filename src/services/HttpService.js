7fac787e7944b6ddecf81fed648f08e25c02ca69
Fady ElHalfawy <fadyhalfawy@gmail.com>
2023-05-16 09:32:49 +0300

    Add toast to the file httpService

--- src/services/HttpService.js
+++ src/services/HttpService.js
@@ -6,12 +6,20 @@ Axios.interceptors.response.use(null, error => {
         && error.response.status >= 400
         && error.response.status < 500;
 
-    if (expectedError){
-        toast.error("An unexpected error occurred..(Wrong username or password)");
-    }
+    if (expectedError) toast.error("An unexpected error occurred..");
 
     return Promise.reject(error);
 });
+
+export const displayNotification = () => toast.success("Logged In or Signed Up Successful");
+export const displayLoginNotification = () => toast.success("Logged In Successful");
+
+export const displayUpdateNotification = () => toast.success("Update User Successful");
+
+export const displayDeleteNotification = () => toast.error("Delete User Successful");
+
+export const displayGetPersonNotification = () => toast.success("Get Person Successful");
+export const displayLogOutNotification = () => toast.success("Logged Out Successful");
 
 function setJwt(jwt) {
     Axios.defaults.headers.common["x-auth-token"] = jwt;
