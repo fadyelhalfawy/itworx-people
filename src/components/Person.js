0958436e66d8838c041e7b7c6f0bfb6cba7db3c3
Fady ElHalfawy <fadyhalfawy@gmail.com>
2023-05-16 08:00:58 +0300

    Update Person Form by adding new button to show single person.

--- src/components/Person.js
+++ src/components/Person.js
@@ -2,7 +2,7 @@ import {Component} from "react";
 import "../css/Person.css";
 export default class Person extends Component {
     render() {
-        const { people, showPerson, deletePerson } = this.props;
+        const { people, showPerson, updatePerson, deletePerson } = this.props;
         return (
             <div className={"people"}>
                 {people.map(person => (
@@ -11,7 +11,8 @@ export default class Person extends Component {
                         <div>
                             <h4>{person.first_name + " " + person.last_name}</h4>
                             <h4>{person.email}</h4>
-                            <button className={"btn btn-outline-info btn-space"} onClick={() => showPerson(person.id)}>Update</button>
+                            <button className={"btn btn-outline-info btn-space m-2"} onClick={showPerson}>Read More</button>
+                            <button className={"btn btn-outline-info btn-space m-2"} onClick={() => updatePerson(person.id)}>Update</button>
                             <button className={"btn btn-outline-info btn-space m-2"} onClick={() => deletePerson(person.id)}>Delete</button>
                         </div>
                     </div>
